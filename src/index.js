22b2ff710c25582b1c7ab3771a7482aa895b1769
Vaibhavi Rohilla <62430272+vaibhavirohilla741@users.noreply.github.com>
2022-07-05 16:22:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 
 ReactDOM.render(
   
